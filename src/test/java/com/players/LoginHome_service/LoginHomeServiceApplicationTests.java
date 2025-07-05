c8fc046311afbc7565fc42dd842ce4ee5a5e7225
vedasree <anapved8@gmail.com>
2025-07-05 21:01:59 +0530

    Initial commit

---
+++ src/test/java/com/players/LoginHome_service/LoginHomeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.players.LoginHome_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginHomeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
