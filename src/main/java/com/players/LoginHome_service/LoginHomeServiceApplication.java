df99bf7a687f444350790a8f5f5658c5e49f01c1
vedasree <anapved8@gmail.com>
2025-08-24 15:44:33 +0530

    Created JWT signup and login features

--- src/main/java/com/players/LoginHome_service/LoginHomeServiceApplication.java
+++ src/main/java/com/players/LoginHome_service/LoginHomeServiceApplication.java
@@ -2,10 +2,11 @@ package com.players.LoginHome_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableDiscoveryClient/*enough since we want this to be discovered not calling any other one*/
 public class LoginHomeServiceApplication {
 
 	public static void main(String[] args) {
